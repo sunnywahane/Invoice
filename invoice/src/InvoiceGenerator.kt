6e5e3639d5ea34139f99d7b40c18c3553db196bb
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-23 18:25:18 +0530

    Updated the implementation of calculateFair to return the total cost of a list of Rides

--- invoice/src/InvoiceGenerator.kt
+++ invoice/src/InvoiceGenerator.kt
@@ -2,8 +2,7 @@ const val DISTANCE_RATE = 10
 const val TIME_RATE = 1
 
 class InvoiceGenerator {
-    fun calculateFair(distance: Double, time: Double): Double {
-        return distance* DISTANCE_RATE + time* TIME_RATE
+    fun calculateFair(Rides: List<Ride>): Double {
+        return Rides.sumOf { it.distance * DISTANCE_RATE + it.time * TIME_RATE }
     }
-
 }
