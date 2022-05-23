f4fe69529f2d08d297f3aa75ba8b4f53cd7b197c
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-23 17:28:34 +0530

    Added unit test to get the far for a list of Rides

--- invoice/test/InvoiceGeneratorTest.kt
+++ invoice/test/InvoiceGeneratorTest.kt
@@ -5,15 +5,15 @@ class InvoiceGeneratorTest : StringSpec() {
     init {
 
         "total for 0 kms and 0 mi̦nutes should be Rs.0" {
-            InvoiceGenerator().calculateFair(0.0,0.0) shouldBeExactly  0.0
+            val rideOne = Ride(0.0, 0.0)
+            InvoiceGenerator().calculateFair(listOf<Ride>(rideOne)) shouldBeExactly 0.0
         }
 
-        "total for 1 kms and 1 minutes should be Rs.11" {
-            InvoiceGenerator().calculateFair(1.0,1.0) shouldBeExactly  11.0
-        }
-
-        "total for 0 kms and 10 minutes should be Rs.10" {
-            InvoiceGenerator().calculateFair(0.0,10.0) shouldBeExactly  10.0
+        "total for 1 kms and 21 mi̦nutes should be Rs.21" {
+            val rideOne = Ride(0.0, 0.0)
+            val rideTwo = Ride(1.0, 1.0)
+            val rideThree  = Ride(0.0, 10.0)
+            InvoiceGenerator().calculateFair(listOf(rideOne, rideTwo, rideThree)) shouldBeExactly  21.0
         }
     }
 }
