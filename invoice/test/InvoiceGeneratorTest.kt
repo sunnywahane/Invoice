8d2956918801c51c4bd3cde4e3bea9d2450f9ff0
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-24 10:16:18 +0530

    Added 2 more test cases

--- invoice/test/InvoiceGeneratorTest.kt
+++ invoice/test/InvoiceGeneratorTest.kt
@@ -15,5 +15,21 @@ class InvoiceGeneratorTest : StringSpec() {
             val rideThree  = Ride(0.0, 10.0)
             InvoiceGenerator().calculateFair(listOf(rideOne, rideTwo, rideThree)) shouldBeExactly  21.0
         }
+
+        "total for 1kms and 11 mi̦nutes should be Rs.21" {
+            val rideOne = Ride(0.0, 10.0)
+            val rideTwo = Ride(1.0, 1.0)
+            InvoiceGenerator().calculateFair(listOf(rideOne, rideTwo)) shouldBeExactly  21.0
+        }
+
+        "total for 1.32kms and 33 mi̦nutes should be Rs.46.2" {
+            val rideOne = Ride(0.32, 10.0)
+            val rideTwo = Ride(1.0, 23.0)
+            InvoiceGenerator().calculateFair(listOf(rideOne, rideTwo)) shouldBeExactly  46.2
+        }
+
+
+
+
     }
 }
