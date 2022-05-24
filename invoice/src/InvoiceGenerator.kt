70c4c22791cbef4a4e017c6b006a6b5741803b1a
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-24 12:30:07 +0530

    Modified the total fair function to replace the lambda function to call the calculateFair function

--- invoice/src/InvoiceGenerator.kt
+++ invoice/src/InvoiceGenerator.kt
@@ -1,8 +1,5 @@
-const val DISTANCE_RATE = 10
-const val TIME_RATE = 1
-
 class InvoiceGenerator {
-    fun calculateFair(Rides: List<Ride>): Double {
-        return Rides.sumOf { it.distance * DISTANCE_RATE + it.time * TIME_RATE }
+    fun calculateTotalFair(Rides: List<Ride>): Double {
+        return Rides.sumOf { it.calculateFair() }
     }
 }
