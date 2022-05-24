b14449656e1fd4877f4178ab5d21d92ebc3127aa
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-24 12:28:56 +0530

    Moved the calulateFair function to the Ride class

--- invoice/src/Ride.kt
+++ invoice/src/Ride.kt
@@ -1,1 +1,7 @@
-data class Ride(val distance: Double, val time: Double)
+const val DISTANCE_RATE = 10
+const val TIME_RATE = 1
+class Ride(val distance: Double, val time: Double) {
+    fun calculateFair(): Double {
+        return this.distance * DISTANCE_RATE + this.time * TIME_RATE
+    }
+}
